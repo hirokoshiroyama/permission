b3fa1f8647cf0c47d811ae7d978878389d5c2d38
hirokoshiroyama <inai.inai.baa777@gmail.com>
2020-07-18 15:52:12 +0900

    パーミッション

--- app/src/main/java/com/example/permission/MainActivity.kt
+++ app/src/main/java/com/example/permission/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.permission
 
+import android.Manifest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val PERMISSIONS_REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("ANDROID", "許可されている")
+                } else {
+                    Log.d("ANDROID", "許可されていない")
+                    requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode:Int,permissions:Array<String>,grantResults:IntArray) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("ANDROID", "許可された")
+                } else {
+                    Log.d("ANDROID", "許可されなかった")
+                }
+        }
     }
 }
